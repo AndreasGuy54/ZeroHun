03e9bee80a49a94114fcb226410eb0ab810e8bab
Andreas Abaidoo <abaidooandy@gmail.com>
2022-10-27 22:35:26 +0000

    Added functions to display the numbers

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-//Call Hello World
-const helloWorld = () => {
-    alert('Hello World!');
+// get the values from the inputs on the app page
+const getValues = () => {
+    let firstNum = parseInt(document.querySelector('#startValue').value)
+    let lastNum = parseInt(document.querySelector('#endValue').value)
+
+    let numbers = generateNumbers(firstNum, lastNum);
+
+    displayNumbers(numbers);
+}
+// generate numbers from the start value to the end value
+const generateNumbers = (startVal, endVal) => {
+    let nums = [];
+
+    for (let i = startVal; i <= endVal; i++) {
+        nums.push(i);
+    }
+    return nums;
+}
+
+// Display the numbers and make even numbers bold
+const displayNumbers = (nums) => {
+    let templateRows = '';
+
+    nums.forEach(num => {
+        let className = '';
+
+        if (num % 2 === 0) {
+            className = 'even';
+        } 
+        
+        templateRows += `<tr><td class="${className}">${num}</td></tr>`;
+    });
+
+    document.querySelector('#results').innerHTML = templateRows;
 }
